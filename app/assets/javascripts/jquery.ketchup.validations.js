b82a906c4458a6bc0072a34d6fb91f6afe2f118c
raphaelrms <raphaelm@gmail.com>
2014-02-07 21:17:09 -0200

    por Raphael Menezes: Atualizando ketchup

--- app/assets/javascripts/jquery.ketchup.validations.js
+++ app/assets/javascripts/jquery.ketchup.validations.js
@@ -1,87 +1,102 @@
 jQuery.ketchup
 
-.validation('required', 'Este campo é obrigatório.', function(form, el, value) {
-  var type = el.attr('type').toLowerCase();
-  
-  if(type == 'checkbox' || type == 'radio') {
-    return (el.attr('checked') == true);
-  } else {
-    return (value.length != 0);
-  }
-})
+    .validation('required', 'Este campo é obrigatório.', function(form, el, value) {
+        var type = el.attr('type');
+        if (typeof type !== 'undefined') {
+            type = type.toLowerCase();
+        }
 
-.validation('minlength', 'Campo deve possuir o mínimo de {arg1} caracteres.', function(form, el, value, min) {
-  return (value.length >= +min);
-})
+        if (type == 'checkbox' || type == 'radio') {
+            return (el.attr('checked') == true);
+        }
+        else {
+            return ($.trim(value).length > 0);
+        }
+    })
 
-.validation('maxlength', 'Campo deve possuir o máximo de {arg1} caracteres.', function(form, el, value, max) {
-  return (value.length <= +max);
-})
+    .validation('token_input', 'Este campo é obrigatório.', function(form, el, value) {
+        el.css('display','block').css('visibility','hidden');
+        el.attr('token_input', true);
+        return el.val() != '';
+    })
 
-.validation('rangelength', 'Valores permitidos: Entre {arg1} e {arg2}.', function(form, el, value, min, max) {
-  return (value.length >= min && value.length <= max);
-})
+    .validation('minlength', 'Campo deve possuir o mínimo de {arg1} caracteres.', function(form, el, value, min) {
+        return (value.length >= +min);
+    })
 
-.validation('min', 'Mínimo de {arg1}.', function(form, el, value, min) {
-  return (this.isNumber(value) && +value >= +min);
-})
+    .validation('maxlength', 'Campo deve possuir o máximo de {arg1} caracteres.', function(form, el, value, max) {
+        return (value.length <= +max);
+    })
 
-.validation('max', 'Máximo de {arg1}.', function(form, el, value, max) {
-  return (this.isNumber(value) && +value <= +max);
-})
+    .validation('rangelength', 'Valores permitidos: Entre {arg1} e {arg2}.', function(form, el, value, min, max) {
+        return (value.length >= min && value.length <= max);
+    })
 
-.validation('range', 'Deve estar entre {arg1} e {arg2}.', function(form, el, value, min, max) {
-  return (this.isNumber(value) && +value >= +min && +value <= +max);
-})
+    .validation('min', 'Mínimo de {arg1}.', function(form, el, value, min) {
+        return (this.isNumber(value) && +value >= +min);
+    })
 
-.validation('number', 'Deve ser um número.', function(form, el, value) {
-  return this.isNumber(value);
-})
+    .validation('max', 'Máximo de {arg1}.', function(form, el, value, max) {
+        return (this.isNumber(value) && +value <= +max);
+    })
 
-.validation('digits', 'Deve conter apenas dígitos.', function(form, el, value) {
-  return /^\d+$/.test(value);
-})
+    .validation('range', 'Deve estar entre {arg1} e {arg2}.', function(form, el, value, min, max) {
+        return (this.isNumber(value) && +value >= +min && +value <= +max);
+    })
 
-.validation('email', 'E-mail inválido.', function(form, el, value) {
-  return this.isEmail(value);
-})
+    .validation('number', 'Deve ser um número.', function(form, el, value) {
+        return this.isNumber(value);
+    })
 
-.validation('url', 'URL inválida.', function(form, el, value) {
-  return this.isUrl(value);
-})
+    .validation('digits', 'Deve conter apenas dígitos.', function(form, el, value) {
+        return /^\d+$/.test(value);
+    })
 
-.validation('username', 'Username inválido.', function(form, el, value) {
-  return this.isUsername(value);
-})
+    .validation('email', 'E-mail inválido.', function(form, el, value) {
+        return this.isEmail(value);
+    })
 
-.validation('match', 'Valor permitido: {arg1}.', function(form, el, value, word) {
-  return (el.val() == word);
-})
+    .validation('emailOpcional', 'E-mail inválido.', function(form, el, value) {
+        return this.isEmail(value) || value == "";
+    })
 
-.validation('contain', 'Deve conter o valor: {arg1}', function(form, el, value, word) {
-  return this.contains(value, word);
-})
+    .validation('url', 'URL inválida.', function(form, el, value) {
+        return this.isUrl(value);
+    })
 
-.validation('date', 'Formato de data inválido.', function(form, el, value) {
-  return this.isDate(value);
-})
+    .validation('username', 'Username inválido.', function(form, el, value) {
+        return this.isUsername(value);
+    })
 
-.validation('minselect', 'Select at least {arg1} checkboxes.', function(form, el, value, min) {
-  return (min <= this.inputsWithName(form, el).filter(':checked').length);
-}, function(form, el) {
-  this.bindBrothers(form, el);
-})
+    .validation('match', 'Valor permitido: {arg1}.', function(form, el, value, word) {
+        return (el.val() == word);
+    })
 
-.validation('maxselect', 'Select not more than {arg1} checkboxes.', function(form, el, value, max) {
-  return (max >= this.inputsWithName(form, el).filter(':checked').length);
-}, function(form, el) {
-  this.bindBrothers(form, el);
-})
+    .validation('contain', 'Deve conter o valor: {arg1}', function(form, el, value, word) {
+        return this.contains(value, word);
+    })
 
-.validation('rangeselect', 'Select between {arg1} and {arg2} checkboxes.', function(form, el, value, min, max) {
-  var checked = this.inputsWithName(form, el).filter(':checked').length;
-  
-  return (min <= checked && max >= checked);
-}, function(form, el) {
-  this.bindBrothers(form, el);
-});
+    .validation('date', 'Formato de data inválido.', function(form, el, value) {
+        var dateRegEx = /^(0[1-9]|[12][0-9]|3[01]|[1-9])[- /.](0[1-9]|1[012]|[1-9])[- /.](19|20)\d\d$/
+        return value.match(dateRegEx) !== null;
+    })
+
+    .validation('minselect', 'Select at least {arg1} checkboxes.', function(form, el, value, min) {
+        return (min <= this.inputsWithName(form, el).filter(':checked').length);
+    }, function(form, el) {
+        this.bindBrothers(form, el);
+    })
+
+    .validation('maxselect', 'Select not more than {arg1} checkboxes.', function(form, el, value, max) {
+        return (max >= this.inputsWithName(form, el).filter(':checked').length);
+    }, function(form, el) {
+        this.bindBrothers(form, el);
+    })
+
+    .validation('rangeselect', 'Select between {arg1} and {arg2} checkboxes.', function(form, el, value, min, max) {
+        var checked = this.inputsWithName(form, el).filter(':checked').length;
+
+        return (min <= checked && max >= checked);
+    }, function(form, el) {
+        this.bindBrothers(form, el);
+    });
